8231308210e7d1d099e45fac77de69eb115df850
pjay <pjay>
2006-12-15 14:56:15 +0000

    added playlists restful route

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,9 @@ ActionController::Routing::Routes.draw do |map|
   # Sample of named route:
   # map.purchase 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
   # This route can be invoked with purchase_url(:id => product.id)
-
+  
+  map.resources :playlists
+  
   # You can have the root of your site routed by hooking up '' 
   # -- just remember to delete public/index.html.
   map.connect '', :controller => 'home', :action => 'index'
