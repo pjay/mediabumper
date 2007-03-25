92838dc71de822e43ba9335b6fac30cd6e128fe9
pjay <pjay>
2007-03-25 13:19:02 +0000

    fixed link_to_playlist helper to call the correct url generator

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   def link_to_playlist(text, streamable)
-    link_to text, url_for_stream(streamable)
+    link_to text, url_for_playlist(streamable)
   end
   
   def url_for_playlist(streamable)
