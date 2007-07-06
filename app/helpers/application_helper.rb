810bd48d27e17f603dcd60975f8d024c8f6ee7bc
pjay <pjay>
2007-07-06 22:05:33 +0000

    added link_to_stream helper and used it in search results

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,23 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-  def link_to_playlist(text, streamable)
-    link_to text, url_for_playlist(streamable)
+  def link_to_playlist(name, streamable)
+    link_to name, url_for_playlist(streamable)
   end
   
   def url_for_playlist(streamable)
     url_for :controller => 'playlists', :action => streamable.class.name.underscore, :id => streamable
   end
   
+  def link_to_stream(name, streamable)
+    link_to_function name, "$('flash_player').loadFile({file:\"#{url_for_stream(streamable)}\"});" + player_event(:playpause)
+  end
+  
   def url_for_stream(streamable)
     url_for :controller => 'files', :action => 'stream', :id => streamable, :filename => streamable.basename
   end
   
-  def link_to_download(text, file_id)
-    link_to text, url_for_download(file_id)
+  def link_to_download(name, file_id)
+    link_to name, url_for_download(file_id)
   end
   
   def url_for_download(file_id)
