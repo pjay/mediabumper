b2e7d5b480c635447b0452db8ac1f37d5664de85
Philippe Jayet <hip@lan-attack.ch>
2007-10-13 23:58:42 +0200

    deleted the old id3 library, updated the TaggedFile abstraction to use ruby-mp3info

--- lib/id3.rb
+++
@@ -1,3 +0,0 @@
-require 'id3/hexdump'
-require 'id3/invert_hash'
-require 'id3/id3'
--- lib/mediabumper/tagged_file.rb
+++ lib/mediabumper/tagged_file.rb
@@ -2,19 +2,11 @@ module Mediabumper
   # ID3 tags parsing library abstraction
   class TaggedFile
     def initialize(path)
-      af = ID3::AudioFile.new path
-      
-      tags1 = af.tagID3v1
-      tags2 = af.tagID3v2
-      
-      ['title', 'artist', 'album', 'year'].each do |key|
-        value = begin
-          tags2[key.upcase]['text'] || tags1[key.upcase]
-        rescue
+      Mp3Info.open(path) do |mp3info|
+        [:title, :artist, :album, :year].each do |key|
+          instance_variable_set :"@#{key}", mp3info.tag.send(key)
+          self.class.send :attr_reader, :"#{key}"
         end
-        
-        instance_variable_set(:"@#{key}", value)
-        self.class.send :attr_reader, :"#{key}"
       end
     end
   end
