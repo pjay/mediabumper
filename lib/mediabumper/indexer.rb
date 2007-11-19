a99d13761b06ad67c52d405eba70539c95c52462
Philippe Jayet <hip@lan-attack.ch>
2007-11-19 19:35:32 +0100

    slightly reworked the indexer

--- lib/mediabumper/indexer.rb
+++ lib/mediabumper/indexer.rb
@@ -1,32 +1,24 @@
 module Mediabumper
   class Indexer
-    class << self
-      def start
-        Repository.find(:all).each do |r|
-          dirs = [r.path]
-          
-          dirs.each do |dir|
-            begin
-              Dir.foreach(dir) do |f|
-                next if f == '.' or f == '..'
+    def initialize(repository, path = nil)
+      @repository = repository
+      @path       = path || @repository.path
+    end
 
-                fullpath = File.join(dir, f)
+    def start
+      dirs = [@path]
 
-                if File.directory? fullpath
-                  dirs << fullpath
-                else
-                  MediaFile.index(fullpath, r)
-                end
-              end
-            rescue Exception => e
-              puts "ERROR: #{e.message}"
-            end
+      while dir = dirs.shift
+        Dir.foreach(dir) do |f|
+          next if f.index('.') == 0  # Skip files/dirs beginning with a dot
+
+          fullpath = File.join(dir, f)
+
+          if File.directory? fullpath
+            dirs << fullpath
+          else
+            MediaFile.index(fullpath, @repository)
           end
-        end
-        
-        # FIXME: there should be a faster way :)
-        MediaFile.find(:all).each do |mf|
-          mf.destroy unless File.exists? mf.path
         end
       end
     end
--- lib/tasks/mediabumper.rake
+++ lib/tasks/mediabumper.rake
@@ -1,6 +1,15 @@
 namespace :mediabumper do
   desc "Starts the indexing of all repositories"
   task :index => :environment do
-    Mediabumper::Indexer.start
+    Repository.find(:all).each do |r|
+      Mediabumper::Indexer.new(r).start
+    end
+
+    # FIXME: there should be a faster way :)
+    MediaFile.find(:all).each do |mf|
+      mf.destroy unless File.exists? mf.path
+    end
+
+    # FIXME: purge other models such as albums, artists and songs
   end
 end
